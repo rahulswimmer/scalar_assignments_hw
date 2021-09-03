db3600772ae12074fba2afe2ff753ca427631f69
rahul <Rahul.Sharma@fusionpractices.com>
2021-09-03 12:32:47 +0530

    initial setup

---
+++ firstRepeatingElement.py
@@ -0,0 +1,7 @@
+class Solution:
+    # @param A : list of integers
+    # @return an integer
+    solve()
+
+    def solve(self, A):
+        print("hello")
---
+++ myfirstprogram.py
@@ -0,0 +1,9 @@
+def main():
+    inputArray = [1,2,3,4,5]
+    rotationFrequency = 1
+    for i in range(rotationFrequency):
+        if
+
+    
+if __name__ == '__main__':
+    main()
