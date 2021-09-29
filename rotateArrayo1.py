dfaf3568d3503a53a96890dea7132864c02bcb21
Rahul Sharma <rahul1406@gmail.com>
2021-09-29 21:46:03 +0530

    new changes

--- rotateArrayo1.py
+++ rotateArrayo1.py
@@ -1,78 +1,46 @@
-# def solve(A,K):
-#     if K>len(A): K=K-len(A)
-
-#     start=0
-#     end=len(A)-1
-#     while start < end:
-#         temp=A[start]
-#         A[start]=A[end]
-#         A[end]=temp
-#         start+=1
-#         end-=1
-  
-#     start=0
-#     end=K-1
-#     while start < end:
-#         temp=A[start]
-#         A[start]=A[end]
-#         A[end]=temp
-#         start+=1
-#         end-=1
-
-#     start=K
-#     end=len(A)-1
-#     while start < end:
-#         temp=A[start]
-#         A[start]=A[end]
-#         A[end]=temp
-#         start+=1
-#         end-=1
-
-#     return A
-# A=[1,2,3,4,5]
-# K=7
-# print(solve(A,K))
 def main():
     A = input().split(" ")
 
     K = int(input())
     n = A[0]
-    
+
     for i in range(1, n):
         ele = int(input())
         A.append(ele)
 
-    if K>len(A): K=K%len(A)
+    if K > len(A):
+        K = K % len(A)
 
-    start=0
-    end=len(A)-1
+    start = 0
+    end = len(A)-1
     while start < end:
-        temp=A[start]
-        A[start]=A[end]
-        A[end]=temp
-        start+=1
-        end-=1
-  
-    start=0
-    end=K-1
-    while start < end:
-        temp=A[start]
-        A[start]=A[end]
-        A[end]=temp
-        start+=1
-        end-=1
+        temp = A[start]
+        A[start] = A[end]
+        A[end] = temp
+        start += 1
+        end -= 1
 
-    start=K
-    end=len(A)-1
+    start = 0
+    end = K-1
     while start < end:
-        temp=A[start]
-        A[start]=A[end]
-        A[end]=temp
-        start+=1
-        end-=1
+        temp = A[start]
+        A[start] = A[end]
+        A[end] = temp
+        start += 1
+        end -= 1
+
+    start = K
+    end = len(A)-1
+    while start < end:
+        temp = A[start]
+        A[start] = A[end]
+        A[end] = temp
+        start += 1
+        end -= 1
 
     for item in A:
-        print(item,end=" ")
+        print(item, end=" ")
+
 
 if __name__ == '__main__':
     main()
