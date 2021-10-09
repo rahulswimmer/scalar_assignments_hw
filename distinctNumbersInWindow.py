465e6e274313238e0c6db8c75ffd0ec51b26fc6d
rahul <Rahul.Sharma@fusionpractices.com>
2021-10-09 18:32:25 +0530

    distinct numbers in window solved

--- distinctNumbersInWindow.py
+++ distinctNumbersInWindow.py
@@ -1,6 +1,7 @@
 def solve(A,B):
     hashmap={}
     ans=0
+    outputArr=[]
 
     for i in range(B):
         if A[i] in hashmap:
@@ -10,17 +11,24 @@ def solve(A,B):
         if hashmap[A[i]]==1:
             ans+=1
     
-    # for i in range(B,len(A)-1):
-    #     hashmap[A[i]]+=1
-    #     hashmap[A[i-B]]-=1
+    outputArr.append(ans)
+
+    for i in range(B,len(A)):
+        if A[i] in hashmap:
+            hashmap[A[i]]+=1
+        else:
+            hashmap[A[i]]=1
+        if hashmap[A[i]]==1:
+            ans+=1
+
+        hashmap[A[i-B]]-=1
         
-    #     if hashmap[A[i]]==1:
-    #         ans+=1
-    #     if hashmap[A[i-B]]==0:
-    #         ans-=1
+        if hashmap[A[i-B]]==0:
+            ans-=1
+        outputArr.append(ans)
 
-    return ans
+    return outputArr
 
-A=[3,7,5,5,4,6,1]
-B=4
+A=[1,2,1,3,4,3]
+B=3
 print(solve(A,B))
